4b29c0f6fd2c1c7b79ac3e52abf8a184962a2ed0
xavilr90 <142954169+xavilr90@users.noreply.github.com>
2023-11-02 16:49:51 -0500

    Update Sonido.java

--- src/Sonido.java
+++ src/Sonido.java
@@ -1,4 +1,12 @@
-
+/*
+ * ------------------------------------------------------------
+ * Name Proyecto: miniProyecto-1
+ * nombre participantes: Xavier , Marvin Nuñez
+ * File: Main.java
+ * Relaciones: clase principal en donde se ejecuta el codigo
+ * Ultima modificacion 06-10-23
+ * ------------------------------------------------------------
+ */
 package Modelo;
 
 import javax.sound.sampled.*;
