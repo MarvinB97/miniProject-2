7b98ba5a145ad8c048fcaa030df794f60dcfed25
xavilr90 <142954169+xavilr90@users.noreply.github.com>
2023-11-02 12:37:22 -0500

    Update Sonido.java

--- src/Sonido.java
+++ src/Sonido.java
@@ -1,1 +1,25 @@
 
+package Modelo;
+
+import javax.sound.sampled.*;
+import java.io.*;
+
+
+public class Sonido {
+
+	public Sonido() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void realizarSonido(String nombreSonido){
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(nombreSonido).getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	        } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+	        System.out.println("Error al reproducir el sonido.");
+	        }
+	     }
+
+}
